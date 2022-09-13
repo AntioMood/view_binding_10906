81eb9e8b435a28912e9e7e3c3aa2e7423a1250a1
AntioMood <brigita079@gmail.com>
2022-09-13 08:33:29 +0700

    View Binding

---
+++ app/src/main/java/com/example/view_binding_10906/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10906
+
+object TaskList {
+    val TaskList = listOf<Task>(
+        Task("Chapter 1","Bangun Tidur","Bangun gegara alarm"),
+        Task("Chapter 2","Mandi","Mandi biar wangi"),
+        Task("Chapter 3","Ke Kampus","Ke kampus biar pinter dikit"),
+        Task("Chapter 4","Selesai Kelas","Selesai kelas nongki sambil chill bersama teman"),
+        Task("Chapter 5","Balik Kos","TURU")
+    )
+}
